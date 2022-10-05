8e0627b5ecd914aafed78419ea607b61ec0c52b1
noah-arbaim <102774780+noah-arbaim@users.noreply.github.com>
2022-10-05 17:04:34 +0900

    fix: Resolved App crash in android
    - Resolved App crash for too large size of product description image by giving an memory saving option on Glide usage at FastImage on Android

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewConverter.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewConverter.java
@@ -1,16 +1,19 @@
 package com.dylanvann.fastimage;
 
-import static com.bumptech.glide.request.RequestOptions.signatureOf;
-
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 
 import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.model.Headers;
 import com.bumptech.glide.load.model.LazyHeaders;
 import com.bumptech.glide.request.RequestOptions;
@@ -19,11 +22,17 @@ import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
 import com.facebook.react.bridge.NoSuchKeyException;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.views.imagehelper.ImageSource;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Nullable;
+
+import static com.bumptech.glide.request.RequestOptions.signatureOf;
 
 class FastImageViewConverter {
     private static final Drawable TRANSPARENT_DRAWABLE = new ColorDrawable(Color.TRANSPARENT);
@@ -49,13 +58,10 @@ class FastImageViewConverter {
                 put("stretch", ScaleType.FIT_XY);
                 put("center", ScaleType.CENTER_INSIDE);
             }};
-
+    
     // Resolve the source uri to a file path that android understands.
-    static @Nullable
-    FastImageSource getImageSource(Context context, @Nullable ReadableMap source) {
-        return source == null
-                ? null
-                : new FastImageSource(context, source.getString("uri"), getHeaders(source));
+    static FastImageSource getImageSource(Context context, ReadableMap source) {
+        return new FastImageSource(context, source.getString("uri"), getHeaders(source));
     }
 
     static Headers getHeaders(ReadableMap source) {
@@ -85,8 +91,8 @@ class FastImageViewConverter {
         // Get cache control method.
         final FastImageCacheControl cacheControl = FastImageViewConverter.getCacheControl(source);
         DiskCacheStrategy diskCacheStrategy = DiskCacheStrategy.AUTOMATIC;
-        boolean onlyFromCache = false;
-        boolean skipMemoryCache = false;
+        Boolean onlyFromCache = false;
+        Boolean skipMemoryCache = false;
         switch (cacheControl) {
             case WEB:
                 // If using none then OkHttp integration should be used for caching.
@@ -102,12 +108,15 @@ class FastImageViewConverter {
         }
 
         RequestOptions options = new RequestOptions()
-                .diskCacheStrategy(diskCacheStrategy)
-                .onlyRetrieveFromCache(onlyFromCache)
-                .skipMemoryCache(skipMemoryCache)
-                .priority(priority)
-                .placeholder(TRANSPARENT_DRAWABLE);
-
+            .diskCacheStrategy(diskCacheStrategy)
+            .onlyRetrieveFromCache(onlyFromCache)
+            .skipMemoryCache(skipMemoryCache)
+            .priority(priority)
+            .placeholder(TRANSPARENT_DRAWABLE)
+//            .fitCenter()
+            .centerInside()
+            .format(DecodeFormat.PREFER_RGB_565);
+        
         if (imageSource.isResource()) {
             // Every local resource (drawable) in Android has its own unique numeric id, which are
             // generated at build time. Although these ids are unique, they are not guaranteed unique
@@ -118,7 +127,7 @@ class FastImageViewConverter {
             options = options.apply(signatureOf(ApplicationVersionSignature.obtain(context)));
         }
 
-        return options;
+        return options;                
     }
 
     private static FastImageCacheControl getCacheControl(ReadableMap source) {
